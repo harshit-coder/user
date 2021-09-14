c04a7fa3abc4a1d27c91eaf45e8ecc5d89d6c5fd
Harshit Kalucha <78843449+harshit-coder@users.noreply.github.com>
2021-09-14 12:31:31 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,21 +1,27 @@
+import json
 from flask import Flask
-
+from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/user')
 def all_users():
-   with open('./sample.json', 'r') as jsonfi:
-      data = json.loads(jsonfi.read())
-      return json.dumps(data)   
-   
-   
-@app.route('/user/user_id')
-def particular_user():
-   user_id = request.values
-   with open('./sample.json', 'r') as jsonfi:
-      data = json.loads(jsonfi.read())
-      return json.dumps(data[user_id])   
-   
+    with open('./sample.json', 'r') as jsonfi:
+        data = json.loads(jsonfi.read())
+        return json.dumps(data)
+
+
+@app.route('/user/<user_id>')
+def particular_user(user_id):
+    id = user_id
+    with open('./sample.json', 'r') as jsonfi:
+        data = json.loads(jsonfi.read())
+        for i in data:
+            if i["id"] == 1:
+                dt = i
+        return json.dumps(dt)
+
+
 if __name__ == '__main__':
     app.run()
