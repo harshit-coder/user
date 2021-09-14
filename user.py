e6084cc90c4d2264cd0a5a209b42687c05574bb5
Harshit Kalucha <78843449+harshit-coder@users.noreply.github.com>
2021-09-14 12:14:34 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,19 +1,21 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route('/user')
 def all_users():
-   data= request.values
-   response = data
-   return jsonify(response)
+   with open('./sample.json', 'r') as jsonfi:
+      data = json.loads(jsonfi.read())
+      return json.dumps(data)   
    
    
 @app.route('/user/user_id')
 def particular_user():
-   data = request.values
-   response = data
-   return jsonify(response)
-
+   user_id = request.values
+   with open('./sample.json', 'r') as jsonfi:
+      data = json.loads(jsonfi.read())
+      return json.dumps(data[user_id])   
+   
 if __name__ == '__main__':
     app.run()
